f0475ebf9699bf27baa2ab517e72b4f919dd21ab
daniatvm <daniatvm@gmail.com>
2020-07-03 22:32:16 -0600

    afinando sección de consejos y puntos

--- server/controllers/AttendantController.js
+++ server/controllers/AttendantController.js
@@ -2,83 +2,98 @@ const db = require('../../database/models');
 
 class AttendantController {
 
-    static async getByCouncil(req,res){
-        try {
-            await db.sequelize.transaction(async t => {
-                const convocados = await db.Convocado.findAll({ where: { consecutivo: req.params.consecutivo } });
-                if (convocados.length > 0) {
-                    res.json({
-                        success: true,
-                        convocados: convocados
-                    });
-                } else {
-                    res.json({
-                        success: false,
-                        msg: 'No se encontraron convocados.'
-                    });
-                }
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
+  static async getByCouncil(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        const convocados = await db.Convocado.findAll({ where: { consecutivo: req.params.consecutivo } });
+        if (convocados.length > 0) {
+          res.json({
+            success: true,
+            convocados: convocados
+          });
+        } else {
+          res.json({
+            success: false,
+            msg: 'No se encontraron convocados.'
+          });
         }
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 
-    static async getByUser(req,res){
-        try {
-            await db.sequelize.transaction(async t => {
-                const convocados = await db.Convocado.findAll({ attributes: ['consecutivo','id_tipo_convocado'], where: { cedula: req.params.cedula } });
-                if (convocados.length > 0) {
-                    //for y recuperar los consejos
-                    res.json({
-                        success: true,
-                        convocados: convocados
-                    });
-                } else {
-                    res.json({
-                        success: false,
-                        msg: 'No se encontraron convocados.'
-                    });
-                }
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
+  static async getByUser(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        const convocados = await db.Convocado.findAll({ attributes: ['consecutivo', 'id_tipo_convocado'], where: { cedula: req.params.cedula } });
+        if (convocados.length > 0) {
+          //for y recuperar los consejos
+          res.json({
+            success: true,
+            convocados: convocados
+          });
+        } else {
+          res.json({
+            success: false,
+            msg: 'No se encontraron convocados.'
+          });
         }
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 
-    static async store(req, res) {
-        const { consecutivo, cedula, id_tipo_convocado } = req.body;
-        try {
-            await db.sequelize.transaction(async t => {
-                await db.Convocado.create({ cedula: cedula, consecutivo: consecutivo, id_tipo_convocado: id_tipo_convocado });
-                res.json({
-                    success: true
-                });
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
-        }
+  static async store(req, res) {
+    const { consecutivo, cedula, id_tipo_convocado } = req.body;
+    try {
+      await db.sequelize.transaction(async t => {
+        await db.Convocado.create({ cedula: cedula, consecutivo: consecutivo, id_tipo_convocado: id_tipo_convocado });
+        res.json({
+          success: true
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 
-    static async remove(req, res) {
-        try {
-            await db.sequelize.transaction(async t => {
-                await db.Convocado.destroy({ where: { cedula: req.params.cedula, consecutivo: req.params.consecutivo } });
-                res.json({
-                    success: true
-                });
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
-        }
+  static async removeByCouncil(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        await db.Convocado.destroy({ where: { consecutivo: req.params.consecutivo } });
+        res.json({
+          success: true
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
+
+  static async remove(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        await db.Convocado.destroy({ where: { cedula: req.params.cedula, consecutivo: req.params.consecutivo } });
+        res.json({
+          success: true
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
+    }
+  }
 }
 
 module.exports = AttendantController;
--- server/controllers/CouncilController.js
+++ server/controllers/CouncilController.js
@@ -2,106 +2,111 @@ const db = require('../../database/models');
 
 class CouncilController {
 
-    static async getCouncils(req, res) {
-        try {
-            await db.sequelize.transaction(async t => {
-                const councils = await db.Consejo.findAll();
-                if (councils.length > 0) {
-                    res.json({
-                        success: true,
-                        councils: councils
-                    });
-                } else {
-                    res.json({
-                        success: false,
-                        msg: 'No se encontraron consejos.'
-                    });
-                }
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
+  static async getCouncils(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        const councils = await db.Consejo.findAll();
+        if (councils.length > 0) {
+          res.json({
+            success: true,
+            councils: councils
+          });
+        } else {
+          res.json({
+            success: false,
+            msg: 'No se encontraron consejos.'
+          });
         }
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 
-    static async store(req, res) {
-        const { consecutivo, id_tipo_sesion, fecha, hora, lugar } = req.body;
-        try {
-            await db.sequelize.transaction(async t => {
-                await db.Consejo.create({
-                    consecutivo: consecutivo, id_tipo_sesion: id_tipo_sesion,
-                    fecha: fecha, hora: hora, lugar: lugar
-                });
-                res.json({
-                    success: true
-                });
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
+  static async store(req, res) {
+    const { consecutivo, id_tipo_sesion, fecha, hora, lugar, puntos } = req.body;
+    try {
+      await db.sequelize.transaction(async t => {
+        await db.Consejo.create({
+          consecutivo: consecutivo, id_tipo_sesion: id_tipo_sesion,
+          fecha: fecha, hora: hora, lugar: lugar
+        });
+        for (let i = 0; i < puntos.length; i++) {
+          await db.Punto.create({
+            asunto: puntos[i], consecutivo: consecutivo
+          });
         }
+        res.json({
+          success: true
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 
-    static async getCouncil(req, res) {
-        try {
-            await db.sequelize.transaction(async t => {
-                const council = await db.Consejo.findOne({
-                    where: { consecutivo: req.params.consecutivo }
-                });
-                if (council) {
-                    res.json({
-                        success: true,
-                        council: council
-                    });
-                } else {
-                    res.json({
-                        success: false,
-                        msg: 'No se encontró el usuario.'
-                    });
-                }
-            })
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.',
-            });
+  static async getCouncil(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        const council = await db.Consejo.findOne({
+          where: { consecutivo: req.params.consecutivo }
+        });
+        if (council) {
+          res.json({
+            success: true,
+            council: council
+          });
+        } else {
+          res.json({
+            success: false,
+            msg: 'No se encontró el usuario.'
+          });
         }
+      })
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.',
+      });
     }
+  }
 
-    static async update(req, res) {
-        const { consecutivo, id_tipo_sesion, fecha, hora, lugar } = req.body;
-        try {
-            await db.sequelize.transaction(async t => {
-                await db.Council.update({
-                    consecutivo: consecutivo, id_tipo_sesion: id_tipo_sesion,
-                    fecha: fecha, hora: hora, lugar: lugar
-                }, { where: { consecutivo: req.params.consecutivo } });
-                res.json({
-                    success: true
-                });
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
-        }
+  static async update(req, res) {
+    const { consecutivo, id_tipo_sesion, fecha, hora, lugar } = req.body;
+    try {
+      await db.sequelize.transaction(async t => {
+        await db.Council.update({
+          consecutivo: consecutivo, id_tipo_sesion: id_tipo_sesion,
+          fecha: fecha, hora: hora, lugar: lugar
+        }, { where: { consecutivo: req.params.consecutivo } });
+        res.json({
+          success: true
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 
-    static async remove(req, res) {
-        try {
-            await db.sequelize.transaction(async t => {
-                await db.Council.destroy({ where: { consecutivo: req.params.consecutivo } });
-                res.json({
-                    success: true
-                });
-            });
-        } catch (error) {
-            res.status(500).json({
-                msg: 'Error interno del servidor.'
-            });
-        }
+  static async remove(req, res) {
+    try {
+      await db.sequelize.transaction(async t => {
+        await db.Consejo.destroy({ where: { consecutivo: req.params.consecutivo } });
+        res.json({
+          success: true
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Error interno del servidor.'
+      });
     }
+  }
 }
 
 module.exports = CouncilController;
