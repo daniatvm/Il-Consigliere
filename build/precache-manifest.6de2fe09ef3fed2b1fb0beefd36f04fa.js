1aef4782fa7499707760db0c5b0da8c2d97353c5
daniatvm <daniatvm@gmail.com>
2020-07-12 20:24:06 -0600

    deploy a heroku con build de react

---
+++ build/precache-manifest.6de2fe09ef3fed2b1fb0beefd36f04fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed91e438dce3ece5dbe2f9c5de8ccc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6920afa73786074bd00f",
+    "url": "/static/css/main.73c8e512.chunk.css"
+  },
+  {
+    "revision": "9d710674b50cb0281ce4",
+    "url": "/static/js/2.02c1befc.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.02c1befc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6920afa73786074bd00f",
+    "url": "/static/js/main.f88cb228.chunk.js"
+  },
+  {
+    "revision": "88af9bb38e705cc04159",
+    "url": "/static/js/runtime-main.71176dca.js"
+  },
+  {
+    "revision": "4b979ea2ec4ecc369ad236a83aa71692",
+    "url": "/static/media/loading.4b979ea2.gif"
+  },
+  {
+    "revision": "692a5d75e7efe7b2609878a02feb1a20",
+    "url": "/static/media/navBrand.692a5d75.png"
+  },
+  {
+    "revision": "0fdff3ece47f0107d2d64868503d2e93",
+    "url": "/static/media/sanjose.0fdff3ec.jpg"
+  }
+]);
