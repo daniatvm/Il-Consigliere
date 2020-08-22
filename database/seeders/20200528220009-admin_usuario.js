e502ede156195db8b66dec3d73619e206957c764
daniatvm <daniatvm@gmail.com>
2020-08-21 22:07:24 -0600

    sistema completado y listo para ser entregado

--- database/seeders/20200528220009-admin_usuario.js
+++ database/seeders/20200528220009-admin_usuario.js
@@ -1,10 +1,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Usuario', [{
-      cedula: '117200790',
-      nombre: 'Daniela',
-      apellido: 'Campos',
-      segundo_apellido: 'Alfaro',
+      cedula: '1234',
+      nombre: 'admin',
+      apellido: 'admin',
+      segundo_apellido: 'admin',
       clave: '$2b$10$UNirhtnN73DPD75jYoh11edJ4HIrxm1GhB8I.bWI2vrBAdDUmWqzC',
       id_tipo_convocado: 3
     }])
